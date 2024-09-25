0ffb97ae8cbb1137c1a93e7053d7169940c5a5e3
MetSpek <daniel@realafford.nl>
2024-09-25 19:24:24 +0200

    Add styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const Example = styled.p`
+  color: blue;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Example>Example Text</Example>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
